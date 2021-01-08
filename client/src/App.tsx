cd634d9b2ca9e1b05016beeb70718dc55d1bb623
Desmond Rogers <desmondrogers2@gmail.com>
2021-01-07 20:35:40 -0600

    Use TypeScript for CRA

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,7 +3,7 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  const [data, setData] = React.useState(null);
+  const [data, setData] = React.useState('');
 
   const getData = () => {
     fetch('/api')
