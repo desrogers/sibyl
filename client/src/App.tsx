41a0545e09bf75424bbada404f69f1413365645b
Desmond Rogers <desmondrogers2@gmail.com>
2021-01-11 16:46:46 -0600

    Add home page redirect to forecast page

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,5 +1,10 @@
 import React from "react";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch,
+  Redirect,
+} from "react-router-dom";
 import Details from "./Details";
 import LandingPage from "./LandingPage";
 
@@ -8,6 +13,9 @@ function App() {
     <Router>
       <Switch>
         <Route exact path="/">
+          <Redirect to="/forecast/40.7127,-74.0059" />
+        </Route>
+        <Route exact path="/forecast/:location">
           <LandingPage />
         </Route>
         <Route exact path="/details/:location">
