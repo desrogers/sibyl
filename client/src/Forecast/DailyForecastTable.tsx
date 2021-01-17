1419b4832e5367c095734edcf793ea888ac0959b
Desmond Rogers <desmondrogers2@gmail.com>
2021-01-16 20:59:27 -0600

    Integrate data with daily forecast table

--- client/src/Forecast/DailyForecastTable.tsx
+++ client/src/Forecast/DailyForecastTable.tsx
@@ -1,3 +1,5 @@
+/// <reference path="Forecast/index.d.ts" /
+
 import {
   Box,
   Table,
@@ -16,7 +18,12 @@ import { WeatherIcon } from "weather-react-icons";
 import KeyboardArrowDownIcon from "@material-ui/icons/KeyboardArrowDown";
 import KeyboardArrowUpIcon from "@material-ui/icons/KeyboardArrowUp";
 import { Link as RouterLink } from "react-router-dom";
-import React, { useState } from "react";
+import React, { useState, useContext } from "react";
+import format from "date-fns/format";
+import addDays from "date-fns/addDays";
+import { Daily } from "ForecastTypes";
+import { AppContext } from "../context";
+const d2d = require("degrees-to-direction");
 
 const useStyles = makeStyles({
   table: {
@@ -28,55 +35,103 @@ const useStyles = makeStyles({
   collapseRow: { display: "flex", justifyContent: "center" },
 });
 
+const transformForecastData = (day: Daily, idx: number) => {
+  const { temp, pop, wind_speed, wind_deg } = day;
+  const date = addDays(new Date(), idx);
+  let relativeDate = format(date, "EEEE");
+
+  if (idx === 0) {
+    relativeDate = "Today";
+  } else if (idx === 1) {
+    relativeDate = "Tomorrow";
+  }
+  return {
+    date: {
+      relative: relativeDate,
+      formatted: format(date, "yyyy-MM-dd"),
+    },
+    temp: `${Math.floor(temp.max)}° / ${Math.floor(temp.min)}°`,
+    precipProb: `${pop * 100}%`,
+    wind: `${d2d(wind_deg)} ${Math.floor(wind_speed)} mph`,
+  };
+};
+
 export default function DailyForecastTable() {
   const classes = useStyles();
   const [open, setOpen] = useState(false);
+  const {
+    state: { forecast },
+  } = useContext(AppContext);
+  const { daily = [], lat = 0, lon = 0 } = forecast;
+
   return (
     <TableContainer component={Paper}>
       <Table className={classes.table} aria-label="daily forecast table">
         <TableBody>
-          <TableRow>
-            <TableCell>{"01/03"}</TableCell>
-            <TableCell>{"59°/37°"}</TableCell>
-            <TableCell>
-              <Box>
-                <WeatherIcon
-                  iconId={701}
-                  name="owm"
-                  className={classes.weatherIcon}
-                />
-                {"Cloudy"}
-              </Box>
-            </TableCell>
-            <TableCell align="center">{"25%"}</TableCell>
-            <TableCell align="right">{"NW 14 mph"}</TableCell>
-            <TableCell align="right">
-              <IconButton
-                aria-label="expand row"
-                size="small"
-                onClick={() => setOpen(!open)}
-              >
-                {open ? <KeyboardArrowUpIcon /> : <KeyboardArrowDownIcon />}
-              </IconButton>
-            </TableCell>
-          </TableRow>
-          <TableRow>
-            <TableCell style={{ paddingBottom: 0, paddingTop: 0 }} colSpan={6}>
-              <Collapse in={open} timeout="auto" unmountOnExit>
-                <Box padding={2} className={classes.collapseRow}>
-                  <Link
-                    component={RouterLink}
-                    to="/details/40.7127,-74.0059/2021-01-03"
-                    underline="none"
+          {daily.map((day, idx) => {
+            const { date, temp, precipProb, wind } = transformForecastData(
+              day,
+              idx
+            );
+
+            return (
+              <>
+                <TableRow>
+                  <TableCell>{date.relative}</TableCell>
+                  <TableCell>{temp}</TableCell>
+                  <TableCell>
+                    <Box>
+                      <WeatherIcon
+                        iconId={day.weather[0].id}
+                        name="owm"
+                        className={classes.weatherIcon}
+                      />
+                      {day.weather[0].main}
+                    </Box>
+                  </TableCell>
+                  <TableCell align="center">{precipProb}</TableCell>
+                  <TableCell align="right">{wind}</TableCell>
+                  <TableCell align="right">
+                    <IconButton
+                      aria-label="expand row"
+                      size="small"
+                      onClick={() => setOpen(!open)}
+                    >
+                      {open ? (
+                        <KeyboardArrowUpIcon />
+                      ) : (
+                        <KeyboardArrowDownIcon />
+                      )}
+                    </IconButton>
+                  </TableCell>
+                </TableRow>
+                <TableRow>
+                  <TableCell
+                    style={{ paddingBottom: 0, paddingTop: 0 }}
+                    colSpan={6}
                   >
-                    <Button variant="outlined" size="small" color="primary">
-                      More Details
-                    </Button>
-                  </Link>
-                </Box>
-              </Collapse>
-            </TableCell>
-          </TableRow>
+                    <Collapse in={open} timeout="auto" unmountOnExit>
+                      <Box padding={2} className={classes.collapseRow}>
+                        <Link
+                          component={RouterLink}
+                          to={`/details/${lat},${lon}/${date.formatted}`}
+                          underline="none"
+                        >
+                          <Button
+                            variant="outlined"
+                            size="small"
+                            color="primary"
+                          >
+                            More Details
+                          </Button>
+                        </Link>
+                      </Box>
+                    </Collapse>
+                  </TableCell>
+                </TableRow>
+              </>
+            );
+          })}
         </TableBody>
       </Table>
     </TableContainer>
